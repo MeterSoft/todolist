aa68aede95a1a6a6cef21540adb80b0a969997ad
Meter <meter@meter-System-Product-Name.(none)>
2012-11-13 14:36:12 +0200

    added ability to create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+     redirect_to users_path
+    else
+      render :action => :new
+    end
+  end
+
 end
